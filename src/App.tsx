3a26398fa09d354b38122129e071cc1d51110d37
FishHeadphones <matthew.ubalde@gmail.com>
2023-11-24 10:50:43 -0500

    Put CardTask in App for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.scss'
+import CardTask from './components/Card/Task/CardTask'
 
 function App() {
   return (
     <>
+      <CardTask id='1' position={0} completed={false} date_modified={Date.now()} body={'Testing testing!'}/>
     </>
   )
 }
