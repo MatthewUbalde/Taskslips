b68892d33cd1919bda1176ad6e22daac63b864bc
FishHeadphones <matthew.ubalde@gmail.com>
2023-11-24 11:11:43 -0500

    Style adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.scss'
-import CardTask from './components/Card/Task/CardTask'
+import "./App.scss";
+import CardTask from "./components/Card/Task/CardTask";
 
 function App() {
   return (
     <>
-      <CardTask id='1' position={0} completed={false} date_modified={Date.now()} body={'Testing testing!'}/>
+      <CardTask
+        id="1"
+        position={0}
+        completed={false}
+        date_modified={Date.now()}
+        body={"Hello! This is a test of what it should look like!"}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card/Task/CardTask.tsx
+++ src/components/Card/Task/CardTask.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import dayjs from 'dayjs';
+import React from "react";
+import dayjs from "dayjs";
 
-import './CardTask.scss'
-import Checkbox from '../Checkbox/Checkbox';
+import "./CardTask.scss";
+import Checkbox from "../Checkbox/Checkbox";
 
-const CardTask = ({id, body, date_modified, completed}: CardTaskData ) => {
+const CardTask = ({ id, body, date_modified, completed }: CardTaskData) => {
   return (
-    <div className='card-task' id={id}>
-      <div className='label'/>
-      <div className='container'>
-        <p className='card-text'>{body}</p>
-        <p className='card-date-modified'>{dayjs(date_modified).format('MMM. DD, YYYY')}</p>
+    <div className="card-task" id={id}>
+      <div className="label" />
+      <div className="container">
+        <p className="card-text">{body}</p>
+        <p className="card-date-modified small-text">
+          {`Created: ${dayjs(date_modified).format("MMM. DD, YYYY")}`}
+        </p>
       </div>
-      <Checkbox isCompleted={completed}/>
+      <Checkbox isCompleted={completed} />
     </div>
   );
-}
+};
 
-export default CardTask
+export default CardTask;
