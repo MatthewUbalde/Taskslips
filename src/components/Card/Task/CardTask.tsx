7275720ab5956c4118cf689d4a4094aa91379386
FishHeadphones <matthew.ubalde@gmail.com>
2023-11-24 10:04:40 -0500

    Created both Cards for Tasks and Titles
    They're the main components that will make up a Folder, which might make up a Binder, which might make up a Project.
    
    Cards > Folder > Binder > Projects
    
    This structure might change though.

---
+++ src/components/Card/Task/CardTask.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./CardTask.scss"
+
+const CardTask = () => {
+return (
+    <div>CardTask</div>
+  )
+}
+
+export default CardTask
---
+++ src/components/Card/Title/CardTitle.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./CardTitle.scss"
+
+const CardTitle = () => {
+  return (
+    <div>CardTitle</div>
+  )
+}
+
+export default CardTitle
