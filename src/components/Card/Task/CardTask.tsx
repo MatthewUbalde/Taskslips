fef09d0d082e4c7dcc8c8248c891c03a1689014c
FishHeadphones <matthew.ubalde@gmail.com>
2023-11-24 10:49:48 -0500

    Used DayJS for date formatting.

--- src/components/Card/Task/CardTask.tsx
+++ src/components/Card/Task/CardTask.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
+import dayjs from 'dayjs';
 
 import './CardTask.scss'
 import Checkbox from '../Checkbox/Checkbox';
 
-
-const CardTask = ({id, position, label_index, body, date_modified, completed}: CardTaskData ) => {
+const CardTask = ({id, body, date_modified, completed}: CardTaskData ) => {
   return (
     <div className='card-task' id={id}>
       <div className='card-label'/>
       <div className='card-body'>
         <p className='card-text'>{body}</p>
-        <p className='card-date-modified'>Nov. 17, 2023</p>
+        <p className='card-date-modified'>{dayjs(date_modified).format('MMM. DD, YYYY')}</p>
       </div>
       <Checkbox isCompleted={completed}/>
     </div>
--- src/lib/types.tsx
+++ src/lib/types.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 type CardTaskData = {
-  id: string,
+  id?: string,
   position: number,
-  body: string,
-  'date-modified': Date,
+  label_index?: number | undefined,
+  body?: string | undefined,
+  date_modified: Date | number, // Number is here due to how Date works!
   completed: boolean
 }
