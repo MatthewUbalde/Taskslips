be75afbe23757d8d5c8ed7506d906f4f40cfba00
FishHeadphones <matthew.ubalde@gmail.com>
2023-11-24 11:48:41 -0500

    Add flex container for the Cards in the Folder

--- src/components/Folder/Folder.tsx
+++ src/components/Folder/Folder.tsx
@@ -7,22 +7,25 @@ const Folder = ({ title, cards }: FolderData) => {
   return (
     <div className="folder">
       <FolderTitle title={title} />
-      {cards?.map(
-        (value: {
-          position: number;
-          date_modified: Date;
-          completed: boolean;
-          children: ReactNode;
-        }) => (
-          <CardTask
-            position={value.position}
-            date_modified={value.date_modified}
-            completed={value.completed}
-          >
-            {value.children}
-          </CardTask>
-        )
-      )}
+
+      <div className="folder-container">
+        {cards?.map(
+          (value: {
+            position: number;
+            date_modified: Date;
+            completed: boolean;
+            children: ReactNode;
+          }) => (
+            <CardTask
+              position={value.position}
+              date_modified={value.date_modified}
+              completed={value.completed}
+            >
+              {value.children}
+            </CardTask>
+          )
+        )}
+      </div>
     </div>
   );
 };
