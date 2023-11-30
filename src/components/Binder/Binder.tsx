730150d6bfc0e62eaa1ee2a878cd608831fc8526
FishHeadphones <matthew.ubalde@gmail.com>
2023-11-30 17:11:33 -0500

    Fix some bugs while publishing it.

--- src/components/Binder/Binder.tsx
+++ src/components/Binder/Binder.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { DndContext, DragEndEvent, closestCorners } from "@dnd-kit/core";
 import { v4 as uuidv4 } from "uuid";
 
